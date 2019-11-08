4092187ba49b4361e3bbd0b333dba234c2dcaf87
Paul Morris <paulh.morris@gmail.com>
2019-11-08 09:52:53 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,137 @@
+//------ Alert Banner ------//
 const alert = document.getElementById('alert');
 
 alert.innerHTML =
   `
   <div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-text"><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
     <p class="alert-banner-close">x</p>
   </div>
   `;
 
 alert.addEventListener('click', e => {
-  const element = e.target;
-  if (element.classList.contains("alert-banner-close")) {
+  if (e.target.classList.contains("alert-banner-close")) {
     alert.style.display = "none";
   }
 });
+
+//------ Messaging Section ------//
+const alertText = document.querySelector('.alert-banner-text');
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+const userList = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
+
+send.addEventListener('click', (e) => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+  e.preventDefault();
+  alert.style.display = "flex";
+  // ensure user and message fields are filled outBack
+  if (user.value === "" && message.value === "") {
+    alertText.textContent = "Please fill out user and message fields before sending";
+  } else if (user.value === "") {
+    alertText.textContent = "Please fill out user field before sending";
+  } else if (message.value === "") {
+    alertText.textContent = "Please fill out message field before sending";
+  } else {
+    alertText.textContent = `Message successfully sent to: ${user.value}`;
+  }
+});
+
+//------ Notification Drop Down ------//
+const bell = document.querySelector('.icon');
+const notif = document.querySelector('.notif');
+
+bell.addEventListener('mousedown', () => {
+  if (notif.style.display === "block") {
+    notif.style.display = "";
+    notif.style.opacity = "0";
+  } else if (notif.style.display === "") {
+    notif.style.display = "block";
+    notif.style.opacity = "1";
+  }
+  bell.classList.add("no-after");
+});
+
+//------ Autocomplete function from "w3schools.com/howto/howto_js_autocomplete.asp" and customized for my project------//
+function autocomplete(inp, arr) {
+  let currentFocus;
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("DIV");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        b.addEventListener("click", function (e) {
+          inp.value = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      currentFocus++;
+      addActive(x);
+    } else if (e.keyCode == 38) { //up
+      currentFocus--;
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+
+autocomplete(document.querySelector('#userField'), userList);
 
 //------ Traffic Chart ------//
 const trafficCanvas = document.getElementById('traffic-chart');
@@ -95,54 +213,46 @@ let trafficChart = new Chart(trafficCanvas, {
 });
 
 //------ Chart Filter Select ------//
+function updateChart(dataset) {
+  // Create new chart
+  let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: dataset,
+    options: trafficOptions
+  });
+  // Animation
+  trafficChart.update({
+    duration: 800,
+    easing: 'easeOutExpo'
+  });
+}
 
-// add event listeners to the .traffic-nav
 const trafficNav = document.getElementById('traffic-nav')
 trafficNav.addEventListener('click', (e) => {
-  trafficChart.destroy();
-  if (e.target.id === 'hourly') {
-    let trafficChart = new Chart(trafficCanvas, {
-      type: 'line',
-      data: hourlyData,
-      options: trafficOptions
-    });
-    trafficChart.update({
-      duration: 700,
-      easing: 'easeOutBack'
-    });
-  }
-  if (e.target.id === 'daily') {
-    let trafficChart = new Chart(trafficCanvas, {
-      type: 'line',
-      data: dayData,
-      options: trafficOptions
-    });
-    trafficChart.update({
-      duration: 800,
-      easing: 'easeOutBack'
-    });
-  }
-  if (e.target.id === 'weekly') {
-    let trafficChart = new Chart(trafficCanvas, {
-      type: 'line',
-      data: weeklyData,
-      options: trafficOptions
-    });
-    trafficChart.update({
-      duration: 800,
-      easing: 'easeOutBack'
-    });
-  }
-  if (e.target.id === 'monthly') {
-    let trafficChart = new Chart(trafficCanvas, {
-      type: 'line',
-      data: monthlyData,
-      options: trafficOptions
-    });
-    trafficChart.update({
-      duration: 800,
-      easing: 'easeOutBack'
-    });
+  // Validate target is not active and is not the parent ul
+  if (!(e.target.classList.contains('active')) &&
+    e.target.id !== 'traffic-nav') {
+
+    // Remove active class (green pill)
+    let activeItem = document.getElementsByClassName('active');
+    activeItem[0].classList.remove('active');
+
+    trafficChart.destroy();
+
+    if (e.target.id === 'hourly') {
+      updateChart(hourlyData);
+    }
+    if (e.target.id === 'daily') {
+      updateChart(dayData);
+    }
+    if (e.target.id === 'weekly') {
+      updateChart(weeklyData);
+    }
+    if (e.target.id === 'monthly') {
+      updateChart(monthlyData);
+    }
+    // Add green pill to new active item
+    e.target.classList.add('active');
   }
 });
 
@@ -212,20 +322,35 @@ let mobileChart = new Chart(mobileCanvas, {
   options: mobileOptions
 });
 
-//------ Messaging Section ------//
-const user = document.getElementById('userField');
-const message = document.getElementById('messageField');
-const send = document.getElementById('send');
+//----- Settings Local Storage -----//
+const email = document.querySelector('#email');
+const public = document.querySelector('#public');
 
-send.addEventListener('click', () => {
-  // ensure user and message fields are filled outBack
-  if (user.value === "" && message.value === "") {
-    alert("Please fill outBack user and message fields before sending");
-  } else if (user.value === "") {
-    alert("Please fill outBack user field before sending");
-  } else if (message.value === "") {
-    alert("Please fill outBack message field before sending");
+window.onload = function() {
+  if (localStorage.email === 'true') {
+    email.checked = true;
   } else {
-    alert(`Message successfully sent to: ${user.value}`);
+    email.checked = false;
+  }
+  if (localStorage.public === 'true') {
+    public.checked = true;
+  } else {
+    public.checked = false;
+  }
+};
+
+email.addEventListener('click', () => {
+  if (email.checked === true) {
+    localStorage.setItem('email', true);
+  } else {
+    localStorage.setItem('email', false);
+  }
+});
+
+public.addEventListener('click', () => {
+  if (public.checked === true) {
+    localStorage.setItem('public', true);
+  } else {
+    localStorage.setItem('public', false);
   }
 });
