75e48d7a87b379ff9d854f010e03c696c32e93b4
Paul Morris <paulh.morris@gmail.com>
2019-12-10 22:02:42 -0600

    Updated project

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
-$( document ).ready(function() {
-  
-  $('body').fadeIn(1200);
+const mw = window.matchMedia("(min-width: 768px)");
+window.addEventListener('resize', resize);
 
-  $('.name-div').slideDown(1000);
-  $('.proj-div').delay(400).slideDown(1000);
-  $('.git-div').delay(800).slideDown(1000);
-  $('.email-div').delay(1200).slideDown(1000);
+function resize() {
+  if (mw.matches) {
+    $('.name-div, .proj-div, .git, .email').fadeIn();
+    $('svg.git-icon').css('opacity', '0');
+  } else {
+    $('.name-div, .proj-div, .git, .email').hide();
+    $('.git-div, .email-div').show();
+    $('svg.git-icon').css('opacity', '1');
+  }
+}
+
+$( document ).ready(function() {
+  $('body').fadeIn(1200);
+  if (mw.matches) {
+    $('.name-div').slideDown(1000);
+    $('.proj-div').delay(400).slideDown(1000);
+    $('.git-div').delay(800).slideDown(1000);
+    $('.email-div').delay(1200).slideDown(1000);
+  } else {
+    $('.git-div, .email-div').show();
+    $('.git, .email').hide();
+  }
 });
+
+$('.profile-pic').hover(function () {
+    $('.hey-there').delay(1000).animate({"opacity":"1"},500);
+  }, function () {
+    $('.hey-there').animate({"opacity":"0"}),500;
+  }
+);
 
 $('.git-div').hover(
   function() {
-    $('.git').animate({"width":"0%"},200);
-    $('.git-icon').animate({"opacity":"1","margin-left":"2rem"},250);
-    },
+    if (mw.matches) {
+      $('.git').animate({"width":"0%"},200);
+      $('.git-icon').animate({"opacity":"1","margin-left":"2.5rem"},250);
+    }
+  },
     function() {
-      $('.git').animate({"width":"100%"},300);
-      $('.git-icon').animate({"opacity":"0","margin-left":"0"},300);
+      if (mw.matches) {
+        $('.git').animate({"width":"100%"},300);
+        $('.git-icon').animate({"opacity":"0","margin-left":"0"},300);
+      }
     }
 );
 
@@ -28,18 +56,17 @@ $('.email-div').hover(
     }
 );
 
-$(".proj-div").click(function (e) { 
+
+$(".proj-div").click(function () { 
   $('.proj-div h2').addClass('active');
   $('.name-div h2').removeClass('active');
   $(".main-wrapper").animate({"left":"-5000px"},700);
   $(".project-wrapper").css("display", "flex").animate({"right":"0"},700);
 });
 
-$(".name-div").click(function (e) { 
+$(".name-div").click(function () { 
   $('.name-div h2').addClass('active');
   $('.proj-div h2').removeClass('active');
   $(".project-wrapper").animate({"right":"-5000px"},700);
   $(".main-wrapper").css("display", "flex").animate({"left":"0px"},700);
 });
-
-$('html').click(function(e) {console.log(e.target)})
